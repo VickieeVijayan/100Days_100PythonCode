a40c42fe5e43e663cfccc9f16cf74cd512074d99
VickieeVijayan <141310727+VickieeVijayan@users.noreply.github.com>
2024-09-22 20:01:18 +0530

    Day1&Day2_Code

---
+++ Day1_BandNameGenerator.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator")
+City_name = input("Which city have you grew up in ? \n ")
+Pet_name = input(" What is your pet name ? \n ")
+
+print("Suggestion for band name " + City_name + " " + Pet_name)
