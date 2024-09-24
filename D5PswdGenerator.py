bfaf508687e47f8a06b22a9eb08adc39583e57cd
Vickiee <VigneshVijayan14@outlook.com>
2024-09-24 19:28:17 +0530

    Day5Code

---
+++ D5PswdGenerator.py
@@ -0,0 +1,30 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+List = []
+
+for letter in range(0, nr_letters):
+    List += random.choice(letters)
+for number in range(0, nr_numbers):
+    List += random.choice(numbers)
+for symbol in range(0, nr_symbols):
+    List.append(random.choice(symbols))
+
+print(List)
+random.shuffle(List)
+
+print(List)
+
+password = ""
+for psd in List:
+    password += psd
+
+print(f"Your password is: {password}")
